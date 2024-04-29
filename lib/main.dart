ed73ac6fe1c9cd38b2d87d0ffbc96616c36d8c65
AdrianPalmaSo <adr_extraordinario@hotmail.com>
2024-04-29 12:13:06 -0600

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pantalla principal'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Acción
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RandomWords()),
+                  );
+                },
+                child: const Text(
+                  'Words',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Acción
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RandomNouns()),
+                  );
+                },
+                child: const Text(
+                  'Nouns',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              ]
+        )
+      ),
+    );
+  }
+}
+class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize:18.0 );
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+      ),
+      body: _buildSuggestions(),
+    );
+  } Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  } Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i){
+          if(i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+
+class RandomNounsState extends State<RandomNouns> {
+  final _suggestions = <String>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(String noun) {
+    return ListTile(
+      title: Text(
+        noun,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(nouns.take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+}
+
+class RandomNouns extends StatefulWidget {
+  const RandomNouns({super.key});
+  @override
+  RandomNounsState createState() => RandomNounsState();
+}
+
+
+//  import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//       return MaterialApp(
+//         home: RandomWords(),
+//       );
+//   }
+// }
+//
+// class RandomWordsState extends State<RandomWords>{
+//   final _suggestions = <WordPair>[];
+//   final _biggerFont = const TextStyle(fontSize:18.0 );
+//   @override
+//   Widget build (BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Welcome to Flutter'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   } Widget _buildRow(WordPair pair){
+//     return ListTile(
+//       title: Text(
+//         pair.asPascalCase,
+//         style: _biggerFont,
+//       ),
+//     );
+//   } Widget _buildSuggestions(){
+//     return ListView.builder(
+//         padding: const EdgeInsets.all(16.0),
+//         itemBuilder: (context, i){
+//           if(i.isOdd) return const Divider();
+//
+//           final index = i ~/ 2;
+//           if (index >= _suggestions.length) {
+//             _suggestions.addAll(generateWordPairs().take(10));
+//           }
+//           return _buildRow(_suggestions[index]);
+//         });
+//   }
+// }
+//
+// class RandomWords extends StatefulWidget {
+//   const RandomWords({super.key});
+//   @override
+//   RandomWordsState createState() => RandomWordsState();
+// }
+
